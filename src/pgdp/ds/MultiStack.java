15814e4193d2581a07499d6fbbd0ecee2831f5de
artemis <artemis.in@tum.de>
2023-01-26 11:13:54 +0100

    Reset Exercise

--- src/pgdp/ds/MultiStack.java
+++ src/pgdp/ds/MultiStack.java
@@ -4,137 +4,44 @@ public class MultiStack {
 
 	private final Stack stacks;
 
-	private final RW lock;
-
 	public MultiStack() {
 		stacks = new Stack(1);
-		lock = new RW();
 	}
 
 	public void push(int val) {
-		try {
-			lock.startWrite();
-			stacks.push(val);
-			lock.endWrite();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
+		stacks.push(val);
 	}
 
 	public int pop() {
-		try {
-			lock.startWrite();
-			if (stacks.isEmpty()) {
-				lock.endWrite();
-				return Integer.MIN_VALUE;
-			}
-			int temp = stacks.pop();
-			lock.endWrite();
-			return temp;
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		if (stacks.isEmpty()) {
+			return Integer.MIN_VALUE;
 		}
-		return Integer.MIN_VALUE;
+		return stacks.pop();
 	}
 
 	public int top() {
-		try {
-			lock.startRead();
-			if (stacks.isEmpty()) {
-				return Integer.MIN_VALUE;
-			}
-			lock.endRead();
-			return stacks.top();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		if (stacks.isEmpty()) {
+			return Integer.MIN_VALUE;
 		}
-		return Integer.MIN_VALUE;
+		return stacks.top();
 	}
 
 	public int size() {
-		try {
-			lock.startRead();
-			if (stacks.isEmpty()) {
-				return 0;
-			}
-			lock.endRead();
-			return stacks.size();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		if (stacks.isEmpty()) {
+			return 0;
 		}
-		return Integer.MIN_VALUE;
+		return stacks.size();
 	}
 
 	public int search(int element) {
-		try {
-			lock.startRead();
-			if (stacks.isEmpty()) {
-				return -1;
-			}
-			lock.endRead();
-			return stacks.search(element);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		if (stacks.isEmpty()) {
+			return -1;
 		}
-		return Integer.MIN_VALUE;
+		return stacks.search(element);
 	}
 
 	@Override
 	public String toString() {
 		return stacks.toString();
 	}
-
-	public static void main(String[] args) {
-		MultiStack ms = new MultiStack();
-		Thread pusher = new Thread(
-				() -> {
-					for (int i = 0; i < 10; i++) {
-						ms.push(i);
-					}
-					System.out.println("finished push1");
-				}
-		);
-
-		Thread topper = new Thread(
-				() -> {
-					System.out.println("top: " + ms.top());
-				}
-		);
-
-		Thread popper = new Thread(
-				() -> {
-					System.out.println("pop: " + ms.pop());
-				}
-		);
-
-		Thread sizer = new Thread(
-				() -> {
-					System.out.println("size: " + ms.size());
-				}
-		);
-
-		Thread pusher2 = new Thread(
-				() -> {
-					for (int i = 10; i < 20; i++) {
-						ms.push(i);
-					}
-					System.out.println("finished push2");
-				}
-		);
-
-		Thread searcher = new Thread(
-				() -> {
-					System.out.println("search: " + ms.search(12));
-				}
-		);
-
-		searcher.start();
-		pusher2.start();
-		pusher.start();
-		topper.start();
-		popper.start();
-		sizer.start();
-	}
 }
-
-
--- src/pgdp/ds/RingBuffer.java
+++ src/pgdp/ds/RingBuffer.java
@@ -1,7 +1,6 @@
 package pgdp.ds;
 
 import java.util.Arrays;
-import java.util.concurrent.Semaphore;
 
 public class RingBuffer {
 
@@ -9,16 +8,12 @@ public class RingBuffer {
 	private int in;
 	private int out;
 	private int stored;
-	private Semaphore free;
-	private Semaphore occupied;
 
 	public RingBuffer(int capacity) {
 		mem = new int[capacity];
 		in = 0;
 		out = 0;
 		stored = 0;
-		free = new Semaphore(capacity);
-		occupied = new Semaphore(0);
 	}
 
 	public boolean isEmpty() {
@@ -30,61 +25,44 @@ public class RingBuffer {
 	}
 
 	public void put(int val) throws InterruptedException {
-		free.acquire();
-		synchronized (this) {
-			if (isFull()) {
-				return;
-			}
-			mem[in++] = val;
-			in %= mem.length;
-			stored++;
+		if (isFull()) {
+			return;
 		}
-		occupied.release();
+		mem[in++] = val;
+		in %= mem.length;
+		stored++;
 	}
 
 	public int get() throws InterruptedException {
-		occupied.acquire();
-		int val = Integer.MIN_VALUE;
-		synchronized (this) {
-			if (isEmpty()) {
-				return Integer.MIN_VALUE;
-			}
-			val = mem[out++];
-			out %= mem.length;
-			stored--;
-
+		if (isEmpty()) {
+			return Integer.MIN_VALUE;
 		}
-		free.release();
+		int val = mem[out++];
+		out %= mem.length;
+		stored--;
 		return val;
 	}
 
 	@Override
 	public String toString() {
-		try {
-			occupied.acquire();
-			StringBuilder sb = new StringBuilder();
-			sb.append("RingBuffer := { capacity = ").append(mem.length).append(", out = ").append(out).append(", in = ")
-					.append(in).append(", stored = ").append(stored).append(", mem = ").append(Arrays.toString(mem))
-					.append(", buffer = [");
-			if (!isEmpty()) {
-				if (in >= 0 || in < mem.length) {
-					int i = out;
-					do {
-						sb.append(mem[i]).append(", ");
-						i = (i + 1) % mem.length;
-					} while (i != in);
-					sb.setLength(sb.length() - 2);
-				} else {
-					sb.append("Error: Field 'in' is <").append(in)
-							.append(">, which is out of bounds for an array of length ").append(mem.length);
-				}
+		StringBuilder sb = new StringBuilder();
+		sb.append("RingBuffer := { capacity = ").append(mem.length).append(", out = ").append(out).append(", in = ")
+				.append(in).append(", stored = ").append(stored).append(", mem = ").append(Arrays.toString(mem))
+				.append(", buffer = [");
+		if (!isEmpty()) {
+			if (in >= 0 || in < mem.length) {
+				int i = out;
+				do {
+					sb.append(mem[i]).append(", ");
+					i = (i + 1) % mem.length;
+				} while (i != in);
+				sb.setLength(sb.length() - 2);
+			} else {
+				sb.append("Error: Field 'in' is <").append(in)
+						.append(">, which is out of bounds for an array of length ").append(mem.length);
 			}
-			sb.append("] }");
-			free.release();
-			return sb.toString();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-			return null;
 		}
+		sb.append("] }");
+		return sb.toString();
 	}
 }
